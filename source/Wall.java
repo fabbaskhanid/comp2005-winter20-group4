5f97f30885b75e68eeb82d59df7773281129248b
Rekperion <60749783+Rekperion@users.noreply.github.com>
2020-03-19 15:25:08 -0230

    Update of the Wall class (#48)
    Added display options as well as identification for if wall is normal or a border wall. Not yet tested but should work fine in theory!

--- source/Wall.java
+++ source/Wall.java
@@ -3,32 +3,114 @@ import javax.swing.BorderFactory;
 
 public class Wall extends BoardTile{
 	
-	private int rowIndex;
-	private int colIndex;
+	private boolean normalWall;
+	private boolean border;
 	
-	public Wall(int row, int col)
+	public Wall(int row, int col, String wall)
 	{
 		super(row, col);
+		drawWall(wall);
 	}
-/*	
+	
+	public void drawWall(String wall)
+	{
+		if (wall == "NB") //north border
+		{
+			setWallNorth();
+		}
+		else if (wall == "EB") //east border
+		{
+			setWallEast();
+		}
+		else if (wall == "SB") //south border
+		{
+			setWallSouth();
+		}
+		else if (wall == "WB") //east border
+		{
+			setWallWest();
+		}
+		else if (wall == "NE") //north east wall
+		{
+			setWallNorthEast();
+		}
+		else if (wall == "NW") //north west wall
+		{
+			setWallNorthWest();
+		}
+		else if (wall == "SE") //south east wall
+		{
+			setWallSouthEast();
+		}
+		else if (wall == "SW") //south west wall
+		{
+			setWallSouthWest();
+		}
+		else {
+			return;
+		}
+	}
+	
 	public void setWallNorth()
 	{
-		tile.setBorder(BorderFactory.createMatteBorder(5, 0, 0, 0, Color.BLACK));
+		this.setBorder(BorderFactory.createMatteBorder(8, 0, 0, 0, Color.BLACK));
+		border = true;
 	}
 	
 	public void setWallEast()
 	{
-		tile.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 5, Color.BLACK));
+		this.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 8, Color.BLACK));
+		border = true;
 	}
 	
 	public void setWallSouth()
 	{
-		tile.setBorder(BorderFactory.createMatteBorder(0, 0, 5, 0, Color.BLACK));
+		this.setBorder(BorderFactory.createMatteBorder(0, 0, 8, 0, Color.BLACK));
+		border = true;
 	}
 	
 	public void setWallWest()
 	{
-		tile.setBorder(BorderFactory.createMatteBorder(0, 5, 0, 0, Color.BLACK));
+		this.setBorder(BorderFactory.createMatteBorder(0, 8, 0, 0, Color.BLACK));
+		border = true;
 	}
-	*/
+	
+	public void setWallNorthEast()
+	{
+		this.setBorder(BorderFactory.createMatteBorder(8, 0, 0, 8, Color.BLACK));
+		normalWall = true;
+	}
+	
+	public void setWallNorthWest()
+	{
+		this.setBorder(BorderFactory.createMatteBorder(8, 8, 0, 0, Color.BLACK));
+		normalWall = true;
+	}
+	
+	public void setWallSouthEast()
+	{
+		this.setBorder(BorderFactory.createMatteBorder(0, 0, 8, 8, Color.BLACK));
+		normalWall = true;
+	}
+	
+	public void setWallSouthWest()
+	{
+		this.setBorder(BorderFactory.createMatteBorder(0, 8, 8, 0, Color.BLACK));
+		normalWall = true;
+	}
+	
+	public String returnWall()
+	{
+		if(normalWall == true)
+		{
+			return "Normal Wall";
+		}
+		else if (border == true)
+		{
+			return "Border";
+		}
+		else {
+			return "None";
+		}
+	}
 }
